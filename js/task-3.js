053d9123fe5312194f54f5141385170cec7274d6
Kateryna <anistar293@gmail.com>
2024-07-28 11:44:40 +0200

    js-hw5

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = users => 
-  users.sort((user1, user2) => user2.friends.length - user1.friends.length);
+  users.toSorted((user1, user2) => user2.friends.length - user1.friends.length);
 
 
 console.log(
